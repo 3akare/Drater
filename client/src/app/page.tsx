42e1fd9cc22eeb22bb2bdb62d81fdef723f34b62
3akare <bakaredavid007@gmail.com>
2025-07-02 20:11:26 +0100

    feat[client]: initialize Next.js application with Tailwind CSS and theme provider - Added package.json for project dependencies and scripts. - Created manifest.json for PWA configuration. - Implemented ThemeProvider component for theme management. - Added global styles using Tailwind CSS. - Set up RootLayout component with metadata and theme provider. - Created a basic Home page component. - Configured Tailwind CSS with custom theme and animations. - Added TypeScript configuration for the project.

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import dynamic from 'next/dynamic'
+
+// const GestureRecorder = dynamic(
+//   () => import('@/app/components/GestureRecorder'),
+//   { ssr: false }
+// )
+
+export default function Home() {
+  return <h1>Drater</h1>
+}
